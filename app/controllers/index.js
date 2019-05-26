30f7cee7b6ad613fe599378928bd54e021eceb4d
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-05-26 12:15:30 +0530

    save invitation to firebase

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -8,11 +8,14 @@ export default Controller.extend({
 	isDisabled: not('isValid'),
 	responseMessage: '',
 	actions: {
-
 		saveInvitation() {
-			alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
-			this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-			this.set('emailAddress', '');
+			let emailAddress = this.get('emailAddress');
+			let newInvitation = this.store.createRecord('invitation', { email: emailAddress });
+			newInvitation.save().then(response =>{
+				this.set('responseMessage', `Thank you! We have just saved your email address: ${response.get('id')}`);
+				this.set('emailAddress', '');
+				
+			});
 		}
 	}
 });
