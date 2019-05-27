7b9ce3f67fd525d8b5dd7f17b231a25329eb0d03
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-05-27 20:31:27 +0530

    component form for adding and editing library

--- app/models/library.js
+++ app/models/library.js
@@ -1,7 +1,9 @@
 import DS from 'ember-data';
+import { notEmpty } from '@ember/object/computed';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   address: DS.attr('string'),
-  phone: DS.attr('string')
+  phone: DS.attr('string'),
+  isValid: notEmpty('name')
 });
