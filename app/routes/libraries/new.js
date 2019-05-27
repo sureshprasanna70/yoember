e5c37c4842bf1af498ef4b555249c7fab984cb71
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-05-27 20:35:57 +0530

    setup controller and render correct template

--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -4,12 +4,23 @@ export default Route.extend({
 	model() {
     return this.store.createRecord('library');
   },
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    controller.set('title', 'Create a new library');
+    controller.set('buttonLabel', 'Create');
+  },
+
+  renderTemplate() {
+    this.render('libraries/form');
+  },
 
   actions: {
 
     saveLibrary(newLibrary) {
       newLibrary.save().then(() => this.transitionTo('libraries'));
     },
+
 
     willTransition() {
       this.controller.get('model').rollbackAttributes();
